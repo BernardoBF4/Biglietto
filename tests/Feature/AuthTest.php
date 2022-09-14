377a3b70dd25d26fc6782cfbbf5bd8784372c1a2
BernardoBF4 <bernardo.fantin@gmail.com>
2022-09-14 02:36:33 +0000

    feat(test: auth): adicionados teste de um_usuario_nao_pode_fazer_login_com_senha_errada, um_usuario_recebe_mensagem_de_erro_se_ja_estiver_logado

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -31,6 +31,38 @@ class AuthTest extends TestCase
 
     $response = $this->post('/cms/auth/login', $data);
 
+    $response->assertSessionHas('message', 'Você está logado');
+    $response->assertStatus(302);
+  }
+
+  /** @test */
+  public function um_usuario_nao_pode_fazer_login_com_senha_errada()
+  {
+    $this->withoutExceptionHandling();
+
+    $password = Str::random(10);
+    $user = User::factory()->withPassword(Str::random(10))->create();
+    $data = ['email' => $user->email, 'password' => $password];
+
+    $response = $this->post('/cms/auth/login', $data);
+
+    $response->assertSessionHas('message', 'A senha está incorreta.');
+    $response->assertStatus(302);
+  }
+
+  /** @test */
+  public function um_usuario_recebe_mensagem_de_erro_se_ja_estiver_logado()
+  {
+    $this->withoutExceptionHandling();
+
+    $password = Str::random(10);
+    $user = User::factory()->withPassword($password)->create();
+    $data = ['email' => $user->email, 'password' => $password];
+
+    auth()->login($user);
+    $response = $this->post('/cms/auth/login', $data);
+
+    $response->assertSessionHas('message', 'Você já está logado');
     $response->assertStatus(302);
   }
 }
