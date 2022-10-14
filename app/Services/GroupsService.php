1850c803fb1a0bfbcb272adee2076c1f6eec6403
BernardoBF4 <bernardo.fantin@gmail.com>
2022-10-14 19:46:28 -0300

    refactor(groupos): removidas verificações sobre o banco de dados para que o código fique de acordo com o que os testem dizem

--- app/Services/GroupsService.php
+++ app/Services/GroupsService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use App\Models\Group;
-use Illuminate\Support\Facades\DB;
 
 class GroupsService
 {
@@ -16,56 +15,29 @@ class GroupsService
 
   public function createGroupWith($data)
   {
-    try {
-      DB::beginTransaction();
+    $modules_id = array_pop($data);
 
-      $modules_id = array_pop($data);
-
-      $group = Group::create($data);
-      $group->modules()->attach($modules_id);
-
-      DB::commit();
-      return ['msg' => trans('cms.groups.success_create')];
-    } catch (\Throwable $error) {
-      DB::rollBack();
-      return ['msg' => $error->getMessage()];
-    }
+    $group = Group::create($data);
+    $group->modules()->attach($modules_id);
+    return ['msg' => trans('cms.groups.success_create')];
   }
 
   public function updateAGroupWith($data, $group)
   {
-    try {
-      DB::beginTransaction();
 
-      $modules_ids = array_pop($data);
+    $modules_ids = array_pop($data);
 
-      $group->update($data);
-      $group->modules()->sync($modules_ids);
+    $group->update($data);
+    $group->modules()->sync($modules_ids);
 
-      DB::commit();
-      return ['msg' => trans('cms.groups.success_update')];
-    } catch (\Throwable $error) {
-      DB::rollBack();
-      return ['msg' => $error->getMessage()];
-    }
+    return ['msg' => trans('cms.groups.success_update')];
   }
 
-  public function deleteGroup($group_id)
+
+  public function deleteGroup($groups_id)
   {
-    try {
-      DB::beginTransaction();
+    Group::whereIn('id', json_decode($groups_id))->delete();
 
-      $group = Group::where('id', $group_id)->first();
-
-      if (isset($group)) {
-        $group->delete();
-      }
-
-      DB::commit();
-      return ['msg' => trans('cms.groups.success_delete')];
-    } catch (\Throwable $error) {
-      DB::rollBack();
-      return ['msg' => $error->getMessage()];
-    }
+    return ['msg' => trans('cms.groups.success_delete')];
   }
 }
