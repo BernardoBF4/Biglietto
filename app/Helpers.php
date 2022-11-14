051174b699e1b1199bee97a87db84eda4cef3b99
BernardoBF4 <bernardo.fantin@gmail.com>
2022-11-14 20:32:43 -0300

    refactor(helper: cms_response): parâmetros não obrigatórios com valores que facilitam a chamada em casos de sucesso

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,13 +1,13 @@
 <?php
 
 if (!function_exists('cms_response')) {
-  function cms_response($msg, $data = [], $status = null, $http_status = null)
+  function cms_response($msg, $success = true, $http_status = 200, $data = [])
   {
     return [
       'data' => $data,
       'http_status' => $http_status,
       'msg' => $msg,
-      'success' => $status,
+      'success' => $success,
     ];
   }
 }
