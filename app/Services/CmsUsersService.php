0e65384e01f429999d3466004e427eee990e55b9
BernardoBF4 <bernardo.fantin@gmail.com>
2022-11-15 19:25:56 -0300

    fix(cms: user service): nome corrigido

--- app/Services/CmsUsersService.php
+++ app/Services/CmsUsersService.php
@@ -7,7 +7,7 @@ use Exception;
 use Illuminate\Support\Facades\Hash;
 use App\Interfaces\CRUD;
 
-class CmsCmsUsersService implements CRUD
+class CmsUsersService implements CRUD
 {
   private ?int $user_id;
   private ?array $data;
