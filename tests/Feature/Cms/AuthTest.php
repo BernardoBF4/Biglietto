3898271dc8ecfa0caaf4c4e16a8293e0e796287f
BernardoBF4 <bernardo.fantin@gmail.com>
2022-12-21 20:02:55 -0300

    refactor(test: groups, auth): removidas funcções inúteis

--- tests/Feature/Cms/AuthTest.php
+++ tests/Feature/Cms/AuthTest.php
@@ -9,66 +9,58 @@ use Tests\TestCase;
 
 class AuthTest extends TestCase
 {
-  use WithFaker, RefreshDatabase;
+    use WithFaker, RefreshDatabase;
 
-  /** @test */
-  public function a_user_can_access_the_login_page()
-  {
-    $this->withoutExceptionHandling();
+    /** @test */
+    public function a_user_can_access_the_login_page()
+    {
+        $this->get('/cms/login')->assertStatus(200);
+    }
 
-    $this->get('/cms/login')->assertStatus(200);
-  }
+    /** @test */
+    public function a_user_can_login()
+    {
+        $password = $this->faker->password(6, 12);
+        $user = User::factory()->withEncryptedPassword($password)->create();
+        $credentials = ['usu_email' => $user->usu_email, 'usu_password' => $password];
 
-  /** @test */
-  public function a_user_can_login()
-  {
-    $this->withoutExceptionHandling();
+        $this->post(route('cms.auth.log_user'), $credentials);
 
-    $password = $this->faker->password(6, 12);
-    $user = User::factory()->withEncryptedPassword($password)->create();
-    $credentials = ['usu_email' => $user->usu_email, 'usu_password' => $password];
+        $this->assertEquals($user->usu_email, auth()->user()->usu_email);
+    }
 
-    $this->post(route('cms.auth.log_user'), $credentials);
+    /** @test */
+    public function a_user_cant_login_with_wrong_password()
+    {
+        $password = $this->faker->password(6, 12);
+        $user = User::factory()->withEncryptedPassword()->create();
+        $credentials = ['usu_email' => $user->usu_email, 'usu_password' => $password];
 
-    $this->assertEquals($user->usu_email, auth()->user()->usu_email);
-  }
+        $response = $this->post(route('cms.auth.log_user'), $credentials);
 
-  /** @test */
-  public function a_user_cant_login_with_wrong_password()
-  {
-    $this->withoutExceptionHandling();
+        $response->assertSessionHas('response', cms_response(__('auth.error.wrong_password'), false, 400));
+    }
 
-    $password = $this->faker->password(6, 12);
-    $user = User::factory()->withEncryptedPassword()->create();
-    $credentials = ['usu_email' => $user->usu_email, 'usu_password' => $password];
+    /** @test */
+    public function a_user_receives_an_error_message_if_already_logged()
+    {
+        $password = $this->faker->password(6, 12);
+        $user = User::factory()->withEncryptedPassword($password)->create();
+        $credentials = ['usu_email' => $user->usu_email, 'usu_password' => $password];
 
-    $response = $this->post(route('cms.auth.log_user'), $credentials);
+        auth()->login($user);
+        $response = $this->post(route('cms.auth.log_user'), $credentials);
 
-    $response->assertSessionHas('response', cms_response(__('auth.error.wrong_password'), false, 400));
-  }
+        $response->assertSessionHas('response', cms_response(__('auth.error.already_logged'), false, 400));
+    }
 
-  /** @test */
-  public function a_user_receives_an_error_message_if_already_logged()
-  {
-    $this->withoutExceptionHandling();
+    /** @test */
+    public function a_user_receives_an_error_message_if_not_found()
+    {
+        $data = ['usu_email' => $this->faker->safeEmail(), 'usu_password' => $this->faker->password(6, 12)];
 
-    $password = $this->faker->password(6, 12);
-    $user = User::factory()->withEncryptedPassword($password)->create();
-    $credentials = ['usu_email' => $user->usu_email, 'usu_password' => $password];
+        $this->post(route('cms.auth.log_user'), $data);
 
-    auth()->login($user);
-    $response = $this->post(route('cms.auth.log_user'), $credentials);
-
-    $response->assertSessionHas('response', cms_response(__('auth.error.already_logged'), false, 400));
-  }
-
-  /** @test */
-  public function a_user_receives_an_error_message_if_not_found()
-  {
-    $data = ['usu_email' => $this->faker->safeEmail(), 'usu_password' => $this->faker->password(6, 12)];
-
-    $this->post(route('cms.auth.log_user'), $data);
-
-    $this->checkIfSessionErrorMatchesString('usu_email', 'Este e-mail não existe em nosso sistema.');
-  }
+        $this->checkIfSessionErrorMatchesString('usu_email', 'Este e-mail não existe em nosso sistema.');
+    }
 }
--- tests/Feature/Cms/GroupsTest.php
+++ tests/Feature/Cms/GroupsTest.php
@@ -11,126 +11,126 @@ use Tests\TestCase;
 
 class GroupsTest extends TestCase
 {
-  use WithFaker, RefreshDatabase;
+    use WithFaker, RefreshDatabase;
 
-  /** @test */
-  public function unauthenticated_users_are_redirected()
-  {
-    $response = $this->get(route('cms.groups.index'));
+    /** @test */
+    public function unauthenticated_users_are_redirected()
+    {
+        $response = $this->get(route('cms.groups.index'));
 
-    $response->assertRedirect();
-  }
+        $response->assertRedirect();
+    }
 
-  /** @test */
-  public function creating_a_group_puts_a_success_message_on_the_session()
-  {
-    $this->signIn();
+    /** @test */
+    public function creating_a_group_puts_a_success_message_on_the_session()
+    {
+        $this->signIn();
 
-    $group_data = Group::factory()->withModule()->make()->toArray();
+        $group_data = Group::factory()->withModule()->make()->toArray();
 
-    $response = $this->post(route('cms.groups.store'), $group_data);
+        $response = $this->post(route('cms.groups.store'), $group_data);
 
-    $response->assertSessionHas('response', cms_response(__('group.success.create')));
-  }
+        $response->assertSessionHas('response', cms_response(__('group.success.create')));
+    }
 
-  /** @test */
-  public function creating_a_group_persists_its_data_to_the_database()
-  {
-    $this->signIn();
+    /** @test */
+    public function creating_a_group_persists_its_data_to_the_database()
+    {
+        $this->signIn();
 
-    $group_data = Group::factory()->make()->toArray();
-    $modules = ['modules' => Modules::factory(1)->create()->pluck('id')];
+        $group_data = Group::factory()->make()->toArray();
+        $modules = ['modules' => Modules::factory(1)->create()->pluck('id')];
 
-    $this->post(route('cms.groups.store'), array_merge($group_data, $modules));
+        $this->post(route('cms.groups.store'), array_merge($group_data, $modules));
 
-    $this->assertDatabaseHas('groups', $group_data);
-    $this->assertDatabaseCount('modules', 2);
-    $this->assertDatabaseCount('group_modules', 2);
-  }
+        $this->assertDatabaseHas('groups', $group_data);
+        $this->assertDatabaseCount('modules', 2);
+        $this->assertDatabaseCount('group_modules', 2);
+    }
 
-  /** @test */
-  public function creating_a_group_without_modules_put_an_error_message_on_the_session()
-  {
-    $this->signIn();
+    /** @test */
+    public function creating_a_group_without_modules_put_an_error_message_on_the_session()
+    {
+        $this->signIn();
 
-    $group_data = Group::factory()->make()->toArray();
+        $group_data = Group::factory()->make()->toArray();
 
-    $this->post(route('cms.groups.store'), $group_data);
+        $this->post(route('cms.groups.store'), $group_data);
 
-    $this->checkIfSessionErrorMatchesString('modules', 'O grupo precisa de pelo menos um módulo.');
-  }
+        $this->checkIfSessionErrorMatchesString('modules', 'O grupo precisa de pelo menos um módulo.');
+    }
 
-  /** @test */
-  public function updating_a_group_puts_a_success_message_on_the_session()
-  {
-    $this->signIn();
+    /** @test */
+    public function updating_a_group_puts_a_success_message_on_the_session()
+    {
+        $this->signIn();
 
-    $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
-    $group_data = Group::factory()->withModule()->make()->toArray();
+        $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
+        $group_data = Group::factory()->withModule()->make()->toArray();
 
-    $response = $this->patch(route('cms.groups.update', ['group' => $group->gro_id]), $group_data);
+        $response = $this->patch(route('cms.groups.update', ['group' => $group->gro_id]), $group_data);
 
-    $response->assertSessionHas('response', cms_response(__('group.success.update')));
-  }
+        $response->assertSessionHas('response', cms_response(__('group.success.update')));
+    }
 
-  /** @test */
-  public function updating_a_not_found_group_puts_an_error_message_on_the_session()
-  {
-    $this->signIn();
+    /** @test */
+    public function updating_a_not_found_group_puts_an_error_message_on_the_session()
+    {
+        $this->signIn();
 
-    $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
-    $group_data = Group::factory()->withModule()->make()->toArray();
+        $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
+        $group_data = Group::factory()->withModule()->make()->toArray();
 
-    $response = $this->patch(route('cms.groups.update', ['group' => $group->gro_id + 1]), $group_data);
+        $response = $this->patch(route('cms.groups.update', ['group' => $group->gro_id + 1]), $group_data);
 
-    $response->assertSessionHas('response', cms_response(__('group.error.not_found'), false, 400));
-  }
+        $response->assertSessionHas('response', cms_response(__('group.error.not_found'), false, 400));
+    }
 
-  /** @test */
-  public function updating_a_group_persists_its_data_to_the_database_and_removes_the_old_data()
-  {
-    $this->signIn();
+    /** @test */
+    public function updating_a_group_persists_its_data_to_the_database_and_removes_the_old_data()
+    {
+        $this->signIn();
 
-    $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
-    $group_data = Group::factory()->make()->toArray();
-    $modules = ['modules' => Modules::factory(1)->create()->pluck('id')];
+        $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
+        $group_data = Group::factory()->make()->toArray();
+        $modules = ['modules' => Modules::factory(1)->create()->pluck('id')];
 
-    $this->patch(route('cms.groups.update', ['group' => $group->gro_id]), array_merge($group_data, $modules));
+        $this->patch(route('cms.groups.update', ['group' => $group->gro_id]), array_merge($group_data, $modules));
 
-    $this->assertDatabaseHas('groups', $group_data);
-    $this->assertDatabaseMissing('groups', ['gro_name' => $group->gro_name, 'gro_status' => $group->gro_status]);
-  }
+        $this->assertDatabaseHas('groups', $group_data);
+        $this->assertDatabaseMissing('groups', ['gro_name' => $group->gro_name, 'gro_status' => $group->gro_status]);
+    }
 
-  /** @test */
-  public function deleting_groups_puts_a_success_message_on_the_session()
-  {
-    $this->signIn();
+    /** @test */
+    public function deleting_groups_puts_a_success_message_on_the_session()
+    {
+        $this->signIn();
 
-    $groups_id = Group::factory(2)->hasAttached(Modules::factory(), [], 'modules')->create()->pluck('gro_id');
+        $groups_id = Group::factory(2)->hasAttached(Modules::factory(), [], 'modules')->create()->pluck('gro_id');
 
-    $response = $this->delete(route('cms.groups.destroy', ['group' => $groups_id]));
+        $response = $this->delete(route('cms.groups.destroy', ['group' => $groups_id]));
 
-    $response->assertSessionHas('response', cms_response(__('group.success.delete')));
-  }
+        $response->assertSessionHas('response', cms_response(__('group.success.delete')));
+    }
 
-  /** @test */
-  public function deleting_a_group_removes_its_data_from_the_database()
-  {
-    $this->withoutExceptionHandling()->signIn();
+    /** @test */
+    public function deleting_a_group_removes_its_data_from_the_database()
+    {
+        $this->signIn();
 
-    $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
+        $group = Group::factory()->hasAttached(Modules::factory(), [], 'modules')->create();
 
-    $this->delete(route('cms.groups.destroy', ['group' => json_encode([$group->gro_id])]));
+        $this->delete(route('cms.groups.destroy', ['group' => json_encode([$group->gro_id])]));
 
-    $this->assertDatabaseMissing('groups', $group->toArray());
-  }
+        $this->assertDatabaseMissing('groups', $group->toArray());
+    }
 
-  /** @test */
-  public function a_group_has_many_users()
-  {
-    $group = Group::factory()->has(User::factory(2)->withEncryptedPassword(), 'users')->create();
+    /** @test */
+    public function a_group_has_many_users()
+    {
+        $group = Group::factory()->has(User::factory(2)->withEncryptedPassword(), 'users')->create();
 
-    $this->assertInstanceOf(User::class, $group->users[0]);
-    $this->assertInstanceOf(User::class, $group->users[1]);
-  }
+        $this->assertInstanceOf(User::class, $group->users[0]);
+        $this->assertInstanceOf(User::class, $group->users[1]);
+    }
 }
